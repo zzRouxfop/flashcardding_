3e9d1a938035dd7c8065f16a6ba508c79457b9b2
zzRxoufop <shuffleitupyourass@gmail.com>
2024-10-09 17:19:23 -0400

    added new files, text me for questions

--- MakingFlashcardSet.py
+++ MakingFlashcardSet.py
@@ -1,12 +1,23 @@
-def synth(title, a, b, c, d, question):
-    while title == None:
-        #making questions and the four choices
-        question = str(input("question: "))
-        a = str(input("choice A: "))
-        b = str(input("choice B: "))
-        c = str(input("choice C: "))
-        d = str(input("choice D: "))
-        row = list(question, a, b, c, d)
-        #choosing which choice is correct
-        CorrectAnswer = str(input("which choice was the correct answer? [A/B/C/D]: "))
-        CorrectAnswer = CorrectAnswer.lower().strip()
+def synth(a, b, c, d, question):
+    #making row
+    row = [question, a, b, c, d]
+    #choosing which choice is correct
+    CorrectAnswer = str(input("which choice was the correct answer? [A/B/C/D]: "))
+    CorrectAnswer = CorrectAnswer.lower().strip()
+    #putting correct choice on the second item on the list {row}
+    if CorrectAnswer == "b":
+        row.remove(b)
+        row.insert(1, b)
+    elif CorrectAnswer == "c":
+        row.remove(c)
+        row.insert(1, c)
+    elif CorrectAnswer == "d":
+        row.remove(d)
+        row.insert(1, d)
+    #final confirmation
+    print(row, "added!")
+    #output
+    row = ",".join(row)
+    return row
+
+
