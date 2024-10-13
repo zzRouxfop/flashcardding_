b85eeba97bd4663e2770547c4a9a898d02d7dd80
Namanmyxao <vietcongsd999@gmail.com>
2024-10-13 16:50:42 -0400

    Add files via upload

---
+++ Givequestion.py
@@ -0,0 +1,22 @@
+from Takequestion import *
+from MakingFlashcardSet_MainFrame import *
+question_file = open(file, "r")
+question_prompt=[]
+questions=[]
+
+#calling out the question in the designated file
+for question in question_file:
+    question_prompt.append(question)
+
+#assign each question to each correct answer
+for i in range(len(question_prompt)):
+    questions.append(Question(question_prompt[i], synth(a, b, c, d, question)))
+
+#this will run the quizz
+def run_quizz(question):
+    score = 0
+    for question in questions:
+        answer = input(question.prompt)
+        if answer == question.answer:
+            score += 1
+    print("You got" +str(score)+"/"+str(len(questions))+" correct")
